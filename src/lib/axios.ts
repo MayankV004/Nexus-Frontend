f8113bde37ddc36fde3fdeb620a0ef0a76b0a9d3
MayankV004 <abcdef.msverma@gmail.com>
2025-08-29 15:54:24 +0530

    added the Backend Base URL

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',// https://nexus-backend-8fhy.onrender.com   //http://localhost:5000
+  baseURL: 'https://nexus-backend-8fhy.onrender.com',// https://nexus-backend-8fhy.onrender.com   //http://localhost:5000
   withCredentials: true,
   headers:{
     'Content-Type': 'application/json',

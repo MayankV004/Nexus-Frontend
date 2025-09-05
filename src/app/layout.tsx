ae65b52276997f1d7f953e569a57335c8c2c3d30
MayankV004 <abcdef.msverma@gmail.com>
2025-09-05 19:11:12 +0530

    Added Meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,33 @@ import { Toaster } from "@/components/ui/sonner";
 import { ReduxProvider } from "@/providers/ReduxProvider";
 import AuthProvider from "@/providers/AuthProvider";
 import { ThemeProvider } from "@/providers/theme-provider";
+import {Metadata }  from "next";
+
+export const metadata: Metadata = {
+  title: "Nexus - Project Management",
+  description: "Manage your projects efficiently with Nexus.",
+  keywords: ["project management", "nexus", "productivity", "collaboration"],
+  authors: [{ name: "Mayank Verma" }],
+  creator: "MayankV004",
+  publisher: "MayankV004",
+  robots: {
+    index: true,
+    follow: true,
+  },
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://www.mayanknexus.app",
+    title: "Nexus - Project Management",
+    description: "Manage your projects efficiently with Nexus.",
+    siteName: "Nexus",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Nexus - Project Management",
+    description: "Manage your projects efficiently with Nexus.",
+  },
+};
 
 export default function RootLayout({
   children,
